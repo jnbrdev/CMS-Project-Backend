8769057edd76e234b44283e7307a07672d0535f2
Jonieber Dela Victoria <delavictoria12@gmail.com>
2023-03-27 17:32:49 +0800

    Removed Security Guard Model, Updated GET request for users where admin and super admin is not included

--- index.js
+++ index.js
@@ -22,7 +22,6 @@ const requestRouter = require("./routes/request");
 const serviceRouter = require("./routes/service");
 const invoiceRouter = require("./routes/invoice");
 const rateRouter = require("./routes/rate");
-const securityRouter = require("./routes/security");
 const guestRouter = require("./routes/guest");
 
 // Middleware
@@ -33,7 +32,6 @@ app.use("/req", requestRouter);
 app.use("/service", serviceRouter);
 app.use("/invoice", invoiceRouter);
 app.use("/rate", rateRouter);
-app.use("/security", securityRouter);
 app.use("/guest", guestRouter);
 
 db.sequelize.sync().then(() => {
--- routes/users.js
+++ routes/users.js
@@ -3,16 +3,32 @@ const router = express.Router();
 const { Users } = require("../models");
 const bcrypt = require("bcryptjs");
 const { Sequelize } = require('sequelize');
+const { Op } = require('sequelize');
 // Get All Users
 router.get("/getAllUser", async (req, res) => {
-  const listOfUsers = await Users.findAll();
-  res.json(listOfUsers);
+  const status = "Active";
+  const excludedRoles = ["Super Admin", "Admin"];
+  const getAllUser = await Users.findAll({
+    where: {
+      status: status,
+      [Op.and]: [
+        { role: { [Op.notIn]: excludedRoles } },
+        { role: { [Op.ne]: null } }
+      ]
+    },
+    attributes: { exclude: ['password'] }
+  });
+  res.json(getAllUser);
 });
 
 // Get Active User
 router.get("/getActiveUser", async (req, res) => {
-  let status = "Active";
-  let getActiveUser = await Users.findAll({ where: { status: status } });
+  const getActiveUser = await Users.findAll({
+    where: {
+      status: "Active",
+      [Op.not]: [{ role: "Super Admin" }, { role: "Admin" }]
+    },
+  });
   res.json(getActiveUser);
 });
 
@@ -24,6 +40,16 @@ router.get("/getOneUser/:id", async (req, res) => {
 });
 
 // Add User
+const roles = {
+  1: "Super Admin",
+  2: "Admin",
+  3: "Accounting",
+  4: "Unit Owner",
+  5: "Tenant",
+  6: "Security Guard",
+  7: "Agent"
+};
+
 router.post("/addUser", async (req, res) => {
   const {
     email,
@@ -45,34 +71,44 @@ router.post("/addUser", async (req, res) => {
       return res.json({ message: "User with this email already exists! " });
     }
 
-    // Fetch the last invoice number from the database
-    const lastUser = await Users.findOne({
-      order: [["createdAt", "DESC"]],
-    });
-    let lastInvoiceNumber = lastUser ? parseInt(lastUser.invoice_no.slice(3), 10) : 0;
-    lastInvoiceNumber++; // Increment the invoice number
-    const formattedInvoiceNumber = `INV${lastInvoiceNumber.toString().padStart(3, "0")}`;
+    let formattedInvoiceNumber = null;
 
-//Save user
-      const postUser = new Users({
-        email,
-        password: encryptedPassword,
-        full_name,
-        contact_no,
-        birthdate,
-        role,
-        status,
-        invoice_no: formattedInvoiceNumber,
+    // Check if the role is "Unit Owner" or "Tenant"
+    if (roles[role] === "Unit Owner" || roles[role] === "Tenant") {
+      // Fetch the last invoice number from the database
+      const lastUser = await Users.findOne({
+        order: [["createdAt", "DESC"]],
       });
-      
-      await postUser.save();
-      res.json({message: "Added Succesfully"});
-      console.log(postUser);
-     
+      let lastInvoiceNumber = 0;
+      if (lastUser?.invoice_no) {
+        lastInvoiceNumber = parseInt(lastUser.invoice_no.slice(3), 10);
+      }
+      lastInvoiceNumber++; // Increment the invoice number
+      formattedInvoiceNumber = `INV${lastInvoiceNumber.toString().padStart(3, "0")}`;
+    }
 
-    
-    
-  } catch (err) {console.log(err)}
+    // Map the role number to the corresponding name
+    const roleName = roles[role];
+
+    // Save user with the role name instead of the role number
+    const postUser = new Users({
+      email,
+      password: encryptedPassword,
+      full_name,
+      contact_no,
+      birthdate,
+      role: roleName, // Use the mapped role name instead of the role number
+      status,
+      invoice_no: formattedInvoiceNumber,
+    });
+
+    await postUser.save();
+    res.json({message: "Added Successfully"});
+    console.log(postUser);
+
+  } catch (err) {
+    console.log(err)
+  }
 });
 
 //Login User
