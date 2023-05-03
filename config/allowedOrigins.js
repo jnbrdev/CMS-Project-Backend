5289fa4a0ff8b51d7616857acac404aaf3c12518
Jonieber Dela Victoria <delavictoria12@gmail.com>
2023-05-03 16:03:53 +0800

    config

--- config/allowedOrigins.js
+++ config/allowedOrigins.js
@@ -1,3 +1,4 @@
 const allowedOrigins = [
-    'http://localhost:3000'
+    'http://localhost:3000',
+    'http://52.63.84.156:3002'
 ]
--- index.js
+++ index.js
@@ -6,11 +6,25 @@ const bodyParser = require("body-parser");
 const dotEnv = require("dotenv").config()
 const bcrypt = require("bcryptjs")
 const cookieParser = require("cookie-parser");
-const allowedOrigins = require("./config/allowedOrigins")
 
+
+const allowedOrigins = [
+  'http://localhost:3002',
+  'http://localhost',  
+  'http://52.63.84.156', 
+  'http://52.63.84.156:3002', 
+  'http://52.63.84.156:3002/api'
+];
+app.use(cors({
+  origin: allowedOrigins
+}));
+
+app.use((req, res, next) => {
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  next();
+});
 
 app.use(express.json());
-app.use(cors(allowedOrigins));
 app.use(cookieParser());
 app.use(morgan('dev'))
 app.use(bodyParser.urlencoded({extended: true}))
@@ -29,18 +43,18 @@ const guestRouter = require("./routes/guest");
 const billingsRouter = require("./routes/billings");
 
 // Middleware
-app.use("/users", userRouter);
-app.use("/unit", unitRouter);
-app.use("/login", loginRouter);
-app.use("/req", requestRouter);
-app.use("/service", serviceRouter);
-app.use("/invoice", invoiceRouter);
-app.use("/rate", rateRouter);
-app.use("/guest", guestRouter);
-app.use("/billings", billingsRouter);
+app.use("/api/users", userRouter);
+app.use("/api/unit", unitRouter);
+app.use("/api/login", loginRouter);
+app.use("/api/req", requestRouter);
+app.use("/api/service", serviceRouter);
+app.use("/api/invoice", invoiceRouter);
+app.use("/api/rate", rateRouter);
+app.use("/api/guest", guestRouter);
+app.use("/api/billings", billingsRouter);
 
 db.sequelize.sync().then(() => {
   app.listen(3002, () => {
-    console.log("server running on port 3003");
+    console.log("server running on port 3002");
   });
 });
